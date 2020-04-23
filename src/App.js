3c279ec0fabec949edabf4c9e2249285b70bae17
saifbinabdulkareem <saifbinabdulkarim.sk@gmail.com>
2020-04-23 15:06:26 +0500

    Landing pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
+import 'react-native-gesture-handler';
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import SiderDemo from './Components/sider.js';
+import { NavigationContainer } from '@react-navigation/native';
+
 
 function App() {
   return (
+    <NavigationContainer>{
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <SiderDemo />
     </div>
+    }</NavigationContainer>
   );
 }
 
